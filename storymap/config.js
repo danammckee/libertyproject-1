ea9aff2552fdc87088277a6c986ae00d0b1853b2
Hollie Eggleton <hollie.eggleton987@gmail.com>
2021-06-11 11:04:32 -0400

    yeh

--- storymap/config.js
+++ storymap/config.js
@@ -7,7 +7,7 @@ var config = {
   // showPopup: true,
   theme: "liberty",
   use3dTerrain: true,
-  // title: 'The Title Text of this Story',
+  title: "The Title Text of this Story",
   // subtitle: 'A descriptive and interesting subtitle to draw in the reader',
   // byline: 'By a Digital Storyteller',
   footer: "Source: source citations, etc.",
